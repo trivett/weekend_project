118acefec4c58629b0a861f2dcf3139ae76b1771
trivett <trivett@gmail.com>
2014-04-05 14:18:48 -0400

    set up db, isntalled rspec

---
+++ people_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeopleApp::Application.config.secret_key_base = '88db8372034f5ab66c3b4194600d90673b4dca6aa3ef2ef42b9291cd00f711fa66613b07fc652648416476e8df5733639c4d48e88a988fedb533bf43179d7dd1'
