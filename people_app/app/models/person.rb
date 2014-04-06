66c7c6510240c297a76bfbe79dea094bf8296720
trivett <trivett@gmail.com>
2014-04-06 09:42:50 -0400

    people can have a drink, but only if over 21 and not drunk

--- people_app/app/models/person.rb
+++ people_app/app/models/person.rb
@@ -7,4 +7,30 @@ class Person <ActiveRecord::Base
   def birthday
     return self.birthdate.strftime("%B%e, %Y")
   end
+
+  def over21?
+    now = DateTime.now
+    age = now.year - self.birthdate.year
+    age -=1 if(now.yday < self.birthdate.yday)
+
+    if age > 21
+      return true
+    else
+      return false
+    end
+  end
+
+  def have_a_drink
+    if self.over21? && self.drinks <= 2
+      self.drinks += 1
+      return "enjoy"
+    elsif self.over21? && self.drinks >= 3
+      return "Go home you're drunk"
+    elsif !self.over21?
+      "Wait a few years"
+    end
+  end
+
+
+
 end
--- people_app/spec/person_spec.rb
+++ people_app/spec/person_spec.rb
@@ -9,14 +9,23 @@ describe Person do
   it "has a birthday" do
     adam = Person.new(:firstname => "Adam", :lastname => "Bray", :birthdate => Date.new(1981,2,3))
     expect(adam.birthday).to eq("February 3, 1981")
-
   end
+
+  it "can have a drink if over 21" do
+    adam = Person.new(:firstname => "Adam", :lastname => "Bray", :birthdate => Date.new(1981,2,3), :drinks => 1)
+    mike = Person.new(:firstname => "Mike", :lastname => "Bray", :birthdate => Date.new(1981,2,3), :drinks => 3)
+    bob = Person.new(:firstname => "Bob", :lastname => "Bray", :birthdate => Date.new(2000,2,3))
+    expect(adam.have_a_drink).to eq("enjoy")
+    expect(mike.have_a_drink).to eq("Go home you're drunk")
+    expect(bob.have_a_drink).to eq("Wait a few years")
+  end
+
+
+
+
 end
 
 
-
-
-# should return a string that is the object's birthday
 # #have_a_drink
 # if they are over 21 then they can drink and the number stored in the drinks attribute is increased by 1
 # if they are under 21 then the string "Wait a few years" is returned
